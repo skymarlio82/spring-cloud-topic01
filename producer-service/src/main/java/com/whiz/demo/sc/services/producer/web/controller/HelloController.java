846e391762739ac4f701e5c39a36d4dfe16749cd
marlio <jitao.liu82@gmail.com>
2020-06-27 20:31:24 +0800

    updated recently.

--- producer-service/src/main/java/com/whiz/demo/sc/services/producer/web/controller/HelloController.java
+++ producer-service/src/main/java/com/whiz/demo/sc/services/producer/web/controller/HelloController.java
@@ -4,11 +4,19 @@ package com.whiz.demo.sc.services.producer.web.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloController {
 
 	@RequestMapping("/foo")
 	public String foo(String foo) {
+		log.trace("log.trace :: foo = {}", foo);
+		log.debug("log.debug :: foo = {}", foo);
+		log.info("log.info :: foo = {}", foo);
+		log.warn("log.warn :: foo = {}", foo);
+		log.error("log.error :: foo = {}", foo);
 		return "Hello, " + foo + "!";
 	}
 }
